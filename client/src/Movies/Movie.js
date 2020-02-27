dd13dc7e60f847102a332838539986a7567b820e
carlsachs <sachscarl@gmail.com>
2020-02-27 16:11:43 -0500

    MVP

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
 import MovieCard from './MovieCard';
 
-function Movie({ addToSavedList }) {
+function Movie(props, { addToSavedList }) {
   const [movie, setMovie] = useState(null);
   const match = useRouteMatch();
 
@@ -11,7 +11,7 @@ function Movie({ addToSavedList }) {
     axios
       .get(`http://localhost:5000/api/movies/${id}`)
       .then(res => setMovie(res.data))
-      .catch(err => console.log(err.response));
+      .catch(err => console.log(err.res));
   };
 
   const saveMovie = () => {
@@ -26,12 +26,28 @@ function Movie({ addToSavedList }) {
     return <div>Loading movie information...</div>;
   }
 
+  const deleteMovie = e => {
+    e.preventDefault();
+    axios
+      .delete(`http://localhost:5000/api/movies/${match.params.id}`)
+      .then(res => {
+        props.getMovieList();
+      })
+      .catch(err => {
+        console.log("There's an error my guy", err);
+      })
+  }
+
+
   return (
     <div className='save-wrapper'>
       <MovieCard movie={movie} />
 
       <div className='save-button' onClick={saveMovie}>
         Save
+      </div>
+      <div>
+      <button onClick={deleteMovie}><Link to='/'> Remove From Saved </Link></button>
       </div>
     </div>
   );
